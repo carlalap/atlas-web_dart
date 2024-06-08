df69fe297315d2f1cc30f3371b798953e71e246c
carlalap <carlos3_alarcon@hotmail.com>
2024-06-08 09:53:11 -0500

    task 4: Code updated under 5 lines

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,11 +1,4 @@
-/*Task 4. Print string 
-Complete this source code in order to print 3 times a string stored in the variable str, followed by its first 9 characters.
-The output of the program should be:
-3 times the value of str
-followed by a new line and the 9 first characters of str
-followed by a new line
-You are not allowed to use any loops or conditional statement
-Your program should be maximum 5 lines long */
+// Task 4. Print string 
 void main() {
   String str = "Holberton School";
   print((str*3) + "\n${str.substring(0,9)}\n");
