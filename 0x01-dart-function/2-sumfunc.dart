fa146f815afc0ca817652237bcf963f6cc312300
carlalap <carlos3_alarcon@hotmail.com>
2024-06-08 14:50:08 -0500

    task2. Add/Sub added

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,11 @@
+// task 2. Add/Sub
+int add(int a, int b) {
+  return(a + b);
+}
+int sub(int a, int b) {
+  return(a - b);
+}
+String showFunc(int a, int b) {
+  print("Add $a + $b = ${add(a, b)}");
+  return("Sub $a - $b = ${sub(a, b)}");
+}
