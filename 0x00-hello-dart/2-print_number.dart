13cfd2095089e51626826338a135ecc816918a11
carlalap <carlos3_alarcon@hotmail.com>
2024-06-08 09:31:43 -0500

    task2. Print Number added

---
+++ 0x00-hello-dart/2-print_number.dart
@@ -0,0 +1,10 @@
+/* Complete this source code in order to print the integer stored in the variable number, followed by Battery street, followed by a new line.
+The output of the script should be:
+the number, followed by Battery street,
+followed by a new line
+You are not allowed to cast the variable number into a string
+Your code must be 4 lines long */
+void main() {
+  var n = 98;
+  print("$n Battery street\n");
+}
