4374076f9307002975eb5c8370efe10871328c7b
carlalap <carlos3_alarcon@hotmail.com>
2024-06-08 09:05:16 -0500

    task0 adding newline character

--- 0x00-hello-dart/0-hello_holberton.dart
+++ 0x00-hello-dart/0-hello_holberton.dart
@@ -1,5 +1,5 @@
 /* Write a dart program that print Hello Holberton! followed by a new line.
 Use the function print */
 void main() {
-  print('Hello Holberton!');
+  print('Hello Holberton!\n');
 }
