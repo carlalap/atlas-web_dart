09fe8c596885496906726556179145c1837fba28
carlalap <carlos3_alarcon@hotmail.com>
2024-06-08 20:43:31 -0500

    task6. Convert to fahrenheit added

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,7 @@
+// 6-convert-to-fahrenheit.dart file
+List<double> convertToF(List<double> temperaturesInC) {
+  return temperaturesInC.map((celsius) {
+    double fahrenheit = (celsius * 9 / 5) + 32;
+    return double.parse((fahrenheit).toStringAsFixed(2));
+  }).toList();
+}
